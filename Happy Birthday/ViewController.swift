c8703d971b8bfcb795e7ee01be20b3c16a027ccf
benedict.ngwai <benedict.ngwai@gmaial.com>
2020-09-10 14:01:29 +0100

    Initial Commit

---
+++ Happy Birthday/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Happy Birthday
+//
+//  Created by Benedict Ng-Wai on 10/09/2020.
+//  Copyright © 2020 Benedict Ng-Wai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
